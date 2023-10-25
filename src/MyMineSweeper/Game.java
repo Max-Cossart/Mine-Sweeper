6b783bbfe0a23f0da24a769aa7082bc665180c1b
Max-Cossart <mcossart1998@gmail.com>
2023-10-26 09:01:27 +1100

    Clean and optimise AssignNums method, Adds Cascading effect, Adds some colouring

--- src/MyMineSweeper/Game.java
+++ src/MyMineSweeper/Game.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class Game {
 	
+	public static final String BLACK_BACKGROUND = "\u001B[40m";
+	public static final String WHITE_TEXT = "\u001B[37m";
+	
 	public void begin() {
-		System.out.println("Welcome to MineSweeper");
+		System.out.println(BLACK_BACKGROUND + WHITE_TEXT +  "Welcome to MineSweeper");
 	}
 	
 	public void startGame() {
@@ -20,15 +23,14 @@ public class Game {
 		
 		while(true) {
 			
-		String invalidEntry;	
 		
-		int x;
-		int y;
+		int x = 1;
+		int y = 1;
 		
 		
 		if(game.isWon ==  true && game.isDone == true) {
 			System.out.println();
-			System.out.println("Congratulations, you have won the game!");
+			System.out.println(BLACK_BACKGROUND + WHITE_TEXT + "Congratulations, you have won the game!");
 			break;
 		} else if (game.isWon == false && game.isDone == true){
 			System.out.println();
@@ -36,36 +38,26 @@ public class Game {
 			break;
 		}
 		
-		System.out.println("Enter an x Co-Ordinate between 1 - 10");
-		while(!scan.hasNextInt()) {			
-			invalidEntry = scan.nextLine();
-			System.out.println(invalidEntry + " is outside the range, Enter an x Co-Ordinate between 1 - 10");
-		}
+		System.out.println(BLACK_BACKGROUND + WHITE_TEXT + "Enter an x Co-Ordinate between 1 - 10");
+
 		x =  scan.nextInt();
 
 		
-		
-		while(x < 1 || x > 10 || x % 1 != 0) {
-			System.out.println(x + " is outside the range, Enter an x Co-Ordinate between 1 - 10");
+		while(x < 1 || x > 10) {		
+				System.out.println(x + " is outside the range, Enter an x Co-Ordinate between 1 - 10");
 			x =  scan.nextInt();
 		}
 		
 		System.out.println("Enter a y Co-Ordinate between 1 - 10");
 		
-		while(!scan.hasNextInt()) {			
-			System.out.println("ran");
-			invalidEntry = scan.nextLine();
-			System.out.println(invalidEntry + " is outside the range, Enter a y Co-Ordinate between 1 - 10 (this should only run once)");
-		}
 		y =  scan.nextInt();
 
-		
 		while(y < 1 || y > 10) {
 			System.out.println(y + " is outside the range, Enter a y Co-Ordinate between 1 - 10");
 			y =  scan.nextInt();
 		}
 		
-		game.playerTurn(x, y);
+		game.playerTurn(x - 1 , y - 1);
 		game.updateGrid();
 		game.Victory();
 		
--- src/MyMineSweeper/MineSweeper.java
+++ src/MyMineSweeper/MineSweeper.java
@@ -1,9 +1,17 @@
 package MyMineSweeper;
 
+import java.util.Arrays;
+
 public class MineSweeper {
 	
 	private String[][] field = new String[10][10];
 	private String[][] display = new String[10][10];
+	
+	public static final String GREY_BACKGROUND = "\u001B[47m";
+	public static final String BLACK_TEXT = "\u001B[30m";
+	public static final String WHITE_TEXT = "\u001B[37m";
+	
+	private int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 	
 	public Boolean isWon = false;
 	public Boolean isDone = false;
@@ -12,9 +20,10 @@ public class MineSweeper {
 	
 	private String mine = " * ";
 	private String unknown = " ? ";
-	private String edge = "|";
-	private String divider = "+---";
+	private String edge = "|" + BLACK_TEXT;
+	private String divider = "+---" + BLACK_TEXT;
 	
+
 	
 	
 	
@@ -35,7 +44,7 @@ public class MineSweeper {
 		System.out.println();
 		for(int x = 0; x < arr.length; x++) {
 			if(x == 0) {				
-				System.out.println("     1   2   3   4   5   6   7   8   9   10");
+				System.out.println(GREY_BACKGROUND + BLACK_TEXT + "     1   2   3   4   5   6   7   8   9   10");
 			}
 				System.out.println("   " + divider.repeat(arr.length) + "+");
 				
@@ -85,183 +94,256 @@ public class MineSweeper {
 	
 	public void assignNums() {
 		for(int x = 0; x < field.length; x++) {
-			
-			for(int y = 0; y < field.length; y++) {	
+			for(int y = 0; y < field[x].length; y++) {					
 				
-				int count = 0;
-				
-				if(field[x][y] != " * ") {
-					
-					if(x != 0 && y != 0 && x != field.length -1 && y != field[x].length - 1) {
-				
-						if(field[x - 1][y - 1] == " * ") {
-							count++;
-						}
-						if(field[x][y - 1] == " * ") {
-							count++;
-						}
-						if(field[x + 1][y - 1] == " * ") {
-							count++;
-						}
-						if(field[x - 1][y] == " * ") {
-							count++;
-						}
-						if(field[x + 1][y] == " * ") {
-							count++;
-						}
-						if(field[x - 1][y + 1] == " * ") {
-							count++;
-						}
-						if(field[x][y + 1] == " * ") {
-							count++;
-						}
-						if(field[x + 1][y + 1] == " * ") {
-							count++;
-						}
-					}
-					if(x == 0) {
-						if(y == 0) {
-							if(field[x + 1][y] == " * ") {
-								count++;
-							}
-							if(field[x][y + 1] == " * ") {
-								count++;
-							}
-							if(field[x + 1][y + 1] == " * ") {
-								count++;
-							}
-						} else if (y == field[x].length - 1) {
-							if(field[x][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x + 1][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x + 1][y] == " * ") {
-								count++;
-							}
-						} else {
-							if(field[x][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x + 1][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x + 1][y] == " * ") {
-								count++;
-							}
-							if(field[x][y + 1] == " * ") {
-								count++;
-							}
-							if(field[x + 1][y + 1] == " * ") {
-								count++;
-							}
-						}
-					}
-					if (x == field.length - 1) {
-						if(y == 0) {
-							if(field[x - 1][y] == " * ") {
-								count++;
-							}
-							if(field[x - 1][y + 1] == " * ") {
-								count++;
-							}
-							if(field[x][y + 1] == " * ") {
-								count++;
-							}
-						} else if (y == field.length - 1) {
-							if(field[x - 1][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x - 1][y] == " * ") {
-								count++;
-							}
-						} else {
-							if(field[x - 1][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x][y - 1] == " * ") {
-								count++;
-							}
-							if(field[x - 1][y] == " * ") {
-								count++;
-							}
-							if(field[x - 1][y + 1] == " * ") {
-								count++;
-							}
-							if(field[x][y + 1] == " * ") {
-								count++;
-							}
-						}
-					}
-					if(y == 0 && x != 0 && x != field.length-1) {
-						if(field[x - 1][y] == " * ") {
-							count++;
-						}
-						if(field[x + 1][y] == " * ") {
-							count++;
-						}
-						if(field[x - 1][y + 1] == " * ") {
-							count++;
-						}
-						if(field[x][y + 1] == " * ") {
-							count++;
-						}
-						if(field[x + 1][y + 1] == " * ") {
-							count++;
-						}
-					}
-					if(y == field.length - 1 && x != 0 && x != field.length - 1) {
-						if(field[x - 1][y - 1] == " * ") {
-							count++;
-						}
-						if(field[x][y - 1] == " * ") {
-							count++;
-						}
-						if(field[x + 1][y - 1] == " * ") {
-							count++;
-						}
-						if(field[x - 1][y] == " * ") {
-							count++;
-						}
-						if(field[x + 1][y] == " * ") {
-							count++;
-						}
-					}
+				if(field[x][y].equals(" * ")) {
+					continue;
 				}
 				
-				if (field[x][y] != " * ") {					
-					field[x][y] = " " + count + " ";
+				int counter = 0;
+				for(int[] direction : directions) {
+					int newX = x + direction[0];
+					int newY = y + direction[1];
+					if(newX >= 0 && newX <= field.length - 1 && newY >= 0 && newY <= field[x].length - 1) {
+						
+						if(field[newX][newY].equals(mine)) {
+							counter++;
+					}
+				}
+					field[x][y] = " " + counter + " ";
 				}
 			}
 		}
 	}
 	
+	public void assignColours() {
+		
+		for(int x = 0; x < field.length; x++) {
+			for(int y = 0; y < field[x].length; y++) {	
+				if(field[x][y].equals(" 0 ")) { 
+					display[x][y] = display[x][y] + WHITE_TEXT;
+				}
+			}	
+		}
+	}
+	
+	
+//	public void assignNums() {
+//		for(int x = 0; x < field.length; x++) {
+//			
+//			for(int y = 0; y < field.length; y++) {	
+//				
+//				int count = 0;
+//				
+//				if(field[x][y] != " * ") {
+//					
+//					if(x != 0 && y != 0 && x != field.length -1 && y != field[x].length - 1) {
+//				
+//						if(field[x - 1][y - 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x][y - 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x + 1][y - 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x - 1][y] == " * ") {
+//							count++;
+//						}
+//						if(field[x + 1][y] == " * ") {
+//							count++;
+//						}
+//						if(field[x - 1][y + 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x][y + 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x + 1][y + 1] == " * ") {
+//							count++;
+//						}
+//					}
+//					if(x == 0) {
+//						if(y == 0) {
+//							if(field[x + 1][y] == " * ") {
+//								count++;
+//							}
+//							if(field[x][y + 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x + 1][y + 1] == " * ") {
+//								count++;
+//							}
+//						} else if (y == field[x].length - 1) {
+//							if(field[x][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x + 1][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x + 1][y] == " * ") {
+//								count++;
+//							}
+//						} else {
+//							if(field[x][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x + 1][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x + 1][y] == " * ") {
+//								count++;
+//							}
+//							if(field[x][y + 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x + 1][y + 1] == " * ") {
+//								count++;
+//							}
+//						}
+//					}
+//					if (x == field.length - 1) {
+//						if(y == 0) {
+//							if(field[x - 1][y] == " * ") {
+//								count++;
+//							}
+//							if(field[x - 1][y + 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x][y + 1] == " * ") {
+//								count++;
+//							}
+//						} else if (y == field.length - 1) {
+//							if(field[x - 1][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x - 1][y] == " * ") {
+//								count++;
+//							}
+//						} else {
+//							if(field[x - 1][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x][y - 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x - 1][y] == " * ") {
+//								count++;
+//							}
+//							if(field[x - 1][y + 1] == " * ") {
+//								count++;
+//							}
+//							if(field[x][y + 1] == " * ") {
+//								count++;
+//							}
+//						}
+//					}
+//					if(y == 0 && x != 0 && x != field.length-1) {
+//						if(field[x - 1][y] == " * ") {
+//							count++;
+//						}
+//						if(field[x + 1][y] == " * ") {
+//							count++;
+//						}
+//						if(field[x - 1][y + 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x][y + 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x + 1][y + 1] == " * ") {
+//							count++;
+//						}
+//					}
+//					if(y == field.length - 1 && x != 0 && x != field.length - 1) {
+//						if(field[x - 1][y - 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x][y - 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x + 1][y - 1] == " * ") {
+//							count++;
+//						}
+//						if(field[x - 1][y] == " * ") {
+//							count++;
+//						}
+//						if(field[x + 1][y] == " * ") {
+//							count++;
+//						}
+//					}
+//				}
+//				
+//				if (field[x][y] != " * ") {					
+//					field[x][y] = " " + count + " ";
+//				}
+//			}
+//		}
+//	}
+	
 	public void playerTurn(int num1, int num2) {
-		int x = num1 - 1;
-		int y = num2 - 1;
+		int x = num1 ;
+		int y = num2 ;
 		
 		count--;
 		
-//		if(field[x][y].equals(" 0 ") == true) {
-//			if(field[x - 1][y - 1]) {
-//				display[x-1][y-1] = field[x - 1][y - 1];
-//			}
-//		}
 		if(!field[x][y].equals(" * ") == true) {
-			display[x][y] = field[x][y];
+			
+			display[x][y]  = field[x][y];
+			
+			if(field[x][y].equals(" 0 ")) {
+				for(int[] direction : directions) {
+					int newX = x + direction[0];
+					int newY = y + direction[1];
+					if(newX >= 0 && newX <= field.length - 1 && newY >= 0 && newY <= field[x].length - 1) {
+							
+						if(display[newX][newY].equals(" ? ")) {
+							this.playerTurn(newX, newY);
+
+						}
+					}
+				}
+			}
+			
 		}else {
 			isDone = true;
 			display = field;
 		}
 	}
 	
-	public void cascade() {
-		
-	}
+//	public void cascade(int x, int y) {
+//
+//		for(int[] direction : directions) {
+//			int newX = x + direction[0];
+//			int newY = y + direction[1];
+//			if(newX >= 0 && newX <= field.length - 1 && newY >= 0 && newY <= field[x].length - 1) {
+//
+//					
+//				if(display[newX][newY].equals(unknown)) {
+//					this.playerTurn(newX, newY);
+//
+//
+//				}
+//			}
+//		}
+//
+//		
+////		for(int[] direction : directions) {
+////			int newX = x + direction[0];
+////			int newY = y + direction[1];
+////			if(newX >= 0 && newX <= field.length - 1 && newY >= 0 && newY <= field[x].length - 1) {
+////				display[newX][newY] = field[newX][newY];				
+////				if(field[newX][newY].equals(" 0 ")) {
+////					System.out.println("also a zero");
+////				}
+////			}
+////		}
+//		
+//	}
 		
 
 	public void setCount() {
